b7b4233c9c04290840113893e7dec5c91edd1d69
gjburke2 <gjburke2@illinois.edu>
2025-05-18 17:27:00 -0500

    Added file saving to backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
 import logging
+import os
+import shutil
 
 from pydantic import BaseModel
 
-from fastapi import FastAPI
-from fastapi import File, UploadFile
-from fastapi import Response
-#from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, UploadFile, Response
+from fastapi.responses import JSONResponse
 #from fastapi.encoders import jsonable_encoder
 
 logger = logging.getLogger('uvicorn.error')
@@ -13,9 +13,21 @@ logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
+FILES_FOLDER = os.path.join(os.getcwd(), 'files')
+os.makedirs(FILES_FOLDER, exist_ok=True)
+
 @app.post("/upload_files/")
-def upload_files(files: list[UploadFile] = File(...)) -> Response:
+async def upload_files(files: list[UploadFile] = File(...)) -> Response:
+    # Handling the input
+    if not files:
+        return JSONResponse(content={"error" : "No files sent"}, status_code=400)
+
+    # Saving the files
     for file in files:
-        logger.debug(file.filename)
-    
+        # Should proabbly worry about unqiue names, paths, etc eventually
+        file_path = os.path.join(FILES_FOLDER, file.filename)
+
+        with open(file_path, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+            
     return Response()
