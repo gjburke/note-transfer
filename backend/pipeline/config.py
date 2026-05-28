726b8e67a9f40c3e3566a3fb7b3be6ad522e66d6
gjburke2 <gjburke2@illinois.edu>
2026-05-28 15:06:53 -0500

    Starting onxx

--- backend/pipeline/config.py
+++ backend/pipeline/config.py
@@ -2,4 +2,5 @@
 SECTION_MODEL_PATH = r"models/large-section.pt"
 LINE_MODEL_PATH = r"models/by-line.pt"
 BASE_MODEL_NAME = "microsoft/trocr-base-handwritten"
+ONNX_PATH = r"models/onnx_model"
 LORA_PATH = r"models/lora"
